f86e2a1c8da34704549ed5596add591188cfb9b9
Dylan <dylan.wi@entechconsulting.com>
2018-06-26 11:54:17 -0400

    updated ip

--- Adafruit_Python_DHT/examples/httpRequest.py
+++ Adafruit_Python_DHT/examples/httpRequest.py
@@ -1,7 +1,7 @@
 import requests
 #import json
 
-urlPost ="http://184.72.116.35:8080/demo/addTemp"
+urlPost ="http://18.204.21.212:8080/demo/addTemp"
 urlLocal ="http://192.168.0.110:8080/demo/addTemp" 
 
 def postData(json_Data):
